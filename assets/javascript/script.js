e3a63c7e4348602dedc89e391895e067aa9f6f5f
Jody Russell <missjodyrussell@gmail.com>
2019-11-24 09:57:29 -0700

    function to save correct line on button click completed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,40 @@
 /////
-// requirements
+// variables
 /////
 
+
+// calling in moment to be used in several places
+const m = moment();
+
+// variables for on save button click 
+var words;
+var hourInfo;
+
+// variables for color coding
+var timeTest = $(".hour").html();
+var realTime = m.format("hh:mm a");
+console.log("Time test" + timeTest);
+console.log("Current Time" + realTime);
+
+
+/////
+// display 
+/////
+
+
 // Near the top of the calendar, the application should display the current day.
-    // https://momentjs.com/guides/
-    // moment(specific format that we desire)
-    // grab local time of the users machine, and spit it out in the
-    // format that we desire
+// moment(specific format that we desire)
+// grab local date of the users machine, and spit it out in the
+// format that we desire
+console.log(m.format("dddd, MMMM Do"));
+
+$("#currentDay").text(m.format("dddd, MMMM Do"));
 
 
-// The app should display standard business hours (9 a.m. to 5 p.m.). 
-// Each time slot should represent one hour and contain the following:
-// * The time
-// * A field to hold user input
-// * A save button (preferably gold in color)
-// Clicking on the save button will store the time and user input in `localStorage`.
+/////
+// functions
+/////
+
 
 // each hour should be color coded to reflect whether the time slot is in the past, the present, 
 // or the future. This will change depending on the time of day.
@@ -22,68 +42,58 @@
 // * if line is current hour it is green and the line after it is purple
 // * else it is a light lavendar
 
-// testing out the box
-// var words;
-// var saveWords = JSON.parse(localStorage.getItem("test words"));
-// console.log("saveWords: " + saveWords);
-// $("#test").empty();
-// $("#test").val(saveWords);
-// $("#save").click(function(){
-//     console.log("I hear a click");
-//     console.log($("#test").val());
-//     words = $("#test").val();
-//     localStorage.setItem("test words", JSON.stringify(words));
-//     console.log("words :" + words);
-//     saveWords = JSON.parse(localStorage.getItem("test words"));
-//     console.log(saveWords);
-// });
-
-/////
-// jumbotron area
-/////
-
-// variables
-
-const m = moment();
-
-// functions
-
-// function using moment.js to call in the date for the top of the page
-console.log(m.format("dddd, MMMM Do"));
-
-$("#currentDay").text(m.format("dddd, MMMM Do"));
-
-/////
-// dayplanner area
-/////
-
-// variables and what ifs
-
-var words;
-var saveWords = JSON.parse(localStorage.getItem("9 am"));
-$("#nineAm").val("");
-$("#nineAm").val(saveWords);
-
 // if that knows what the current time is and highlights the current hour
 // if current hour, else if current hour +1, else past hour color
 // is ran on each page refresh and each save?
 
-
-// functions
-
-$(".saveBtn").click(function(){
-    words = $("#nineAm").val();
-    localStorage.setItem("9 am", JSON.stringify(words));
-    saveWords = JSON.parse(localStorage.getItem("#nineAm"));
+// THIS FUNCTION IS NOT WORKING THE WAY I THOUGHT IT WOULD
+// ADDITIONALLY IT NEEDS TO BE CALLED ON DOCUMENT LOAD AND
+// ALSO BE AVAILABLE TO BE CALLED ON SAVE
+$(document).ready( function() {
+    $(".input").each(function () {
+    //for each of the hours unique positions apply below
+    // right now it's applying to all
+        if (realTime > timeTest) {
+            $(".input").toggleClass("past");
+            console.log("You're on the right path");
+        } else if (realTime < timeTest) {
+            $(".input").toggleClass("future");
+        } else {
+            $(".input").toggleClass("present");
+        }
+    });
+    
+    // run FUNCTION SAVEWORDS?
 });
 
-// dummy function that allows us to save that row to localStorage
+// FUNCTION SAVEWORDS
+// Need to change these into a dummy function that refreshes each 
+// slot with it's currently stored information
+// var saveWords = JSON.parse(localStorage.getItem("9 am"));
+// $("#nineAm").val("");
+// $("#nineAm").val(saveWords);
+// dummy function that then is called by the first dummy function 
+// and prints the localStorage
+// informaiton to the page
 
 // function that listen for clicks, knows which row the button is connected
 // to, and then runs the dummy function to save the information
+// dummy function that allows us to save that row to localStorag
+// happens on click of any .saveBtn
+$(".saveBtn").click(function() {
+    words = $(this).siblings(".input").val();
+    console.log(words);
+    hourInfo = $(this).siblings(".hour").text();
+    console.log(hourInfo);
+    localStorage.setItem(hourInfo, JSON.stringify(words));
 
-// dummy function that then is called by the first dummy function and prints the localStorage
-// informaiton to the page
+    // run FUNCTION SAVEWORDS
+    // run FUNCTION COLORING
+})
+
+
+
+
 
 
 

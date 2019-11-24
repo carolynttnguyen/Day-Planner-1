42f4587c24c4843516b099fc54cf127fb9e01080
Jody Russell <missjodyrussell@gmail.com>
2019-11-24 16:26:32 -0700

    application completed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,10 +11,6 @@ var words;
 var hourInfo;
 
 // variables for color coding
-var timeTest = $(".hour").html();
-var realTime = m.format("hh:mm a");
-console.log("Time test" + timeTest);
-console.log("Current Time" + realTime);
 
 
 /////
@@ -29,6 +25,8 @@ console.log("Current Time" + realTime);
 console.log(m.format("dddd, MMMM Do"));
 
 $("#currentDay").text(m.format("dddd, MMMM Do"));
+
+
 
 
 /////
@@ -49,26 +47,40 @@ $("#currentDay").text(m.format("dddd, MMMM Do"));
 // THIS FUNCTION IS NOT WORKING THE WAY I THOUGHT IT WOULD
 // ADDITIONALLY IT NEEDS TO BE CALLED ON DOCUMENT LOAD AND
 // ALSO BE AVAILABLE TO BE CALLED ON SAVE
+
 $(document).ready( function() {
-    $(".input").each(function () {
-    //for each of the hours unique positions apply below
-    // right now it's applying to all
-        if (realTime > timeTest) {
-            $(".input").toggleClass("past");
-            console.log("You're on the right path");
-        } else if (realTime < timeTest) {
-            $(".input").toggleClass("future");
-        } else {
-            $(".input").toggleClass("present");
-        }
-    });
-    
-    // run FUNCTION SAVEWORDS?
+    colorChange ();
+    renderText ();
 });
 
-// FUNCTION SAVEWORDS
-// Need to change these into a dummy function that refreshes each 
-// slot with it's currently stored information
+function colorChange () {
+    
+    var realTime = moment().hours();
+    // console.log("Time test" + timeTest);
+    console.log("Current Time" + realTime);
+    
+    //for each of the hours unique positions apply below
+    // right now it's applying to all
+    $(".input").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        console.log(timeTest);
+        
+        if (realTime > timeTest) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (realTime < timeTest) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+}
+
 // var saveWords = JSON.parse(localStorage.getItem("9 am"));
 // $("#nineAm").val("");
 // $("#nineAm").val(saveWords);
@@ -86,12 +98,54 @@ $(".saveBtn").click(function() {
     hourInfo = $(this).siblings(".hour").text();
     console.log(hourInfo);
     localStorage.setItem(hourInfo, JSON.stringify(words));
-
-    // run FUNCTION SAVEWORDS
-    // run FUNCTION COLORING
+    
+    colorChange ();
+    renderText ();
 })
 
+// FUNCTION SAVEWORDS
+// Need to change these into a dummy function that refreshes each 
+// slot with it's currently stored information
 
+// couldn't for the life of me wrap my head around how to make this
+// a dummy function :(
+function renderText () {
+    var saveWords9 = JSON.parse(localStorage.getItem("9:00 am"));
+    $("#9").val("");
+    $("#9").val(saveWords9);
+    
+    var saveWords10 = JSON.parse(localStorage.getItem("10:00 am"));
+    $("#10").val("");
+    $("#10").val(saveWords10);
+    
+    var saveWords11 = JSON.parse(localStorage.getItem("11:00 am"));
+    $("#11").val("");
+    $("#11").val(saveWords11);
+    
+    var saveWords12 = JSON.parse(localStorage.getItem("12:00 pm"));
+    $("#12").val("");
+    $("#12").val(saveWords12);
+    
+    var saveWords1 = JSON.parse(localStorage.getItem("1:00 pm"));
+    $("#13").val("");
+    $("#13").val(saveWords1);
+
+    var saveWords2 = JSON.parse(localStorage.getItem("2:00 pm"));
+    $("#14").val("");
+    $("#14").val(saveWords2);
+
+    var saveWords3 = JSON.parse(localStorage.getItem("3:00 pm"));
+    $("#15").val("");
+    $("#15").val(saveWords3);
+
+    var saveWords4 = JSON.parse(localStorage.getItem("4:00 pm"));
+    $("#16").val("");
+    $("#16").val(saveWords4);
+
+    var saveWords5 = JSON.parse(localStorage.getItem("5:00 pm"));
+    $("#17").val("");
+    $("#17").val(saveWords5);
+}
 
 
 
